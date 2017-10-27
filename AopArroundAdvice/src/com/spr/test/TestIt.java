70b2b4b0dafe0f912e668fb97c9192b87d554aa2
T05049 <T05049@APPSTEK-E01047.appsteksolutions.internal>
2017-10-27 11:25:57 +0530

    jamon example

--- AopArroundAdvice/src/com/spr/test/TestIt.java
+++ AopArroundAdvice/src/com/spr/test/TestIt.java
@@ -2,6 +2,7 @@ package com.spr.test;
 
 import org.springframework.aop.framework.ProxyFactory;
 
+import com.spr.beans.CacheAdvice;
 import com.spr.beans.Calculator;
 import com.spr.beans.LoggingAdvice;
 
@@ -11,11 +12,14 @@ public class TestIt {
 		// TODO Auto-generated method stub
        ProxyFactory ft = new ProxyFactory();
        ft.setTarget(new Calculator());
+       ft.addAdvice(new CacheAdvice());
        ft.addAdvice(new LoggingAdvice());
        
        Calculator proxy = (Calculator)ft.getProxy();
         int result = proxy.add(10,20);
         System.out.println(result);
+        int result1 = proxy.add(15,20);
+        System.out.println(result1);
        
 	}
 
